2139be00d2f2289053dbbffc85de0cc9878f7847
Kestutis <kestutis.kunce@gmail.com>
2018-10-19 22:44:02 +0300

    Fixed gitignore, cleanup

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var app = new express();
 var bodyParser = require('body-parser');
 var itemRoute = require('./app/routes/playgroundItemRoute');
-const port = process.env.PORT || 3001;
+
+var app = new express();
 var router = new express.Router();
+
+const PORT = process.env.PORT || 3001;
 
 itemRoute(router);
 
@@ -12,10 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 app.use(function(req, res) {
-  console.log('Doesnt work!'); 
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
 app.listen(port, function(req, res) {
-  console.log("test");
+  console.log(`App running on port ${PORT}`);
 });
