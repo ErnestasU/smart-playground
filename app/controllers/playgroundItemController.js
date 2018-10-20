aaa1d08cae510a42bf2775bce9f4bd80db37ef69
Martynas Mitrulevičius <martynas.mitrulevicius@spp.se>
2018-10-20 11:59:35 +0300

    added method get trainer by ref

--- app/controllers/playgroundItemController.js
+++ app/controllers/playgroundItemController.js
@@ -1,31 +1,5 @@
 'use strict';
 
-exports.get_item = function (req, res) {
-
-
-    var MongoClient = require('mongodb').MongoClient;
-    var url = process.env.MONGODB_CONNECTION;
-    
-    // res.json({ "itemId": req.params.itemId })
-
-    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("smart-playground-db");
-
-        dbo.collection("smart-playground-collection").findOne({}, function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            db.close();
-            res.json({ "itemId": result.id })
-        });
-    });
-
-};
-
-exports.update_item = function (req, res) {
-    res.json(req.body);
-};
-
 exports.get_all_trainers = function(req, res) {
     var MongoClient = require('mongodb').MongoClient;
     var url = process.env.MONGODB_CONNECTION;
@@ -42,4 +16,18 @@ exports.get_all_trainers = function(req, res) {
     });
 }
 
+exports.get_trainer_by_ref = function(req, res) {
+    var MongoClient = require('mongodb').MongoClient;
+    var url = process.env.MONGODB_CONNECTION;
 
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("smart-playground-db");
+
+        dbo.collection("trainers").findOne({"ref" : req.params.ref}, function (err, result) {
+            if (err) throw err;
+            db.close();
+            res.json(result);
+        });
+    });
+}
--- app/routes/playgroundItemRoute.js
+++ app/routes/playgroundItemRoute.js
@@ -2,13 +2,12 @@
 
 module.exports = function(router) {
     var itemController = require('../controllers/playgroundItemController');
-  
-    router.route('/items/:itemId')
-      .get(itemController.get_item)
-      .put(itemController.update_item);
 
     router.route('/trainers')
         .get(itemController.get_all_trainers);
+
+    router.route('/trainer/:ref')
+        .get(itemController.get_trainer_by_ref);
 
   };
   
